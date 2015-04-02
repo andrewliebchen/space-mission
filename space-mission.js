948c91f6a94dd6eb19dab86ed8049c2f09b07353
andrewliebchen <andrewliebchen@gmail.com>
2015-04-01 22:18:05 -0700

    Basic add system and constellation

--- space-mission.js
+++ space-mission.js
@@ -15,10 +15,46 @@ if (Meteor.isClient) {
       return Systems.find({parent: this._id});
     }
   });
+
+  Template.addConstellation.events({
+    'click .mtr_add-constellation': function() {
+      Meteor.call('addConstellation', {
+        title: 'New constellation',
+        description: 'Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque.'
+      });
+    }
+  });
+
+  Template.addSystem.events({
+    'click .mtr_add-system': function() {
+      Meteor.call('addSystem', {
+        title: 'New system',
+        description: 'Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque.',
+        size: 'small',
+        parent: this._id
+      });
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.methods({
+      addConstellation: function(args){
+        Constellations.insert({
+          title: args.title,
+          description: args.description
+        });
+      },
+
+      addSystem: function(args){
+        Systems.insert({
+          title: args.title,
+          description: args.description,
+          size: args.size,
+          parent: args.parent
+        });
+      }
+    });
   });
 }
